830315b373acac3caef22d25391b2c772d9ec190
elifkurtdereli <132361864+elifkurtdereli@users.noreply.github.com>
2023-06-07 18:29:31 +0300

    Help action'ı ve view'i oluşturuldu.

--- ElmaProjesi/Controllers/HomeController.cs
+++ ElmaProjesi/Controllers/HomeController.cs
@@ -28,5 +28,11 @@ namespace ElmaProjesi.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Help()
+        {
+            return View();
+        }
+
     }
 }
