4b1ebe46e75f7b6664bbaa0b605bd64699679d48
Mustafa Kerem Tay <81324994+keremmtay@users.noreply.github.com>
2023-06-08 17:53:36 +0300

    InitialData eklendi, Entityler düzenlendi, Program.cs düzenlendi, Migrationlar eklendi

--- ElmaProjesi.DataAccessLayer/Migrations/20230607180905_mig2.Designer.cs
+++ ElmaProjesi.DataAccessLayer/Migrations/20230607180905_mig2.Designer.cs
@@ -3,6 +3,7 @@ using ElmaProjesi.DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ElmaProjesi.DataAccessLayer.Migrations
 {
     [DbContext(typeof(ElmaContext))]
-    partial class ElmaContextModelSnapshot : ModelSnapshot
+    [Migration("20230607180905_mig2")]
+    partial class mig2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ElmaProjesi.EntityLayer/Category.cs
+++ ElmaProjesi.EntityLayer/Category.cs
@@ -10,7 +10,6 @@ namespace ElmaProjesi.EntityLayer
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
         public string Url { get; set; }
         public string ImageUrl { get; set; }
         //İlişkiler
